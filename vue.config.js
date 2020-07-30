90afd0e813a4e4f8243c9d4736ba28307a706c82
KINGMJ <328047478@qq.com>
2020-07-30 11:28:09 +0800

    多页面应用配置

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const __appsPath = path.resolve(__dirname, './src/apps')
+
+module.exports = {
+  pages: {
+    board: {
+      entry: path.resolve(__appsPath, './board/index.js')
+    },
+    home: {
+      entry: path.resolve(__appsPath, './home/index.js')
+    },
+    mindmap: {
+      entry: path.resolve(__appsPath, './mindmap/index.js')
+    },
+    profile: {
+      entry: path.resolve(__appsPath, './profile/index.js')
+    },
+    project: {
+      entry: path.resolve(__appsPath, './project/index.js')
+    }
+  }
+}
