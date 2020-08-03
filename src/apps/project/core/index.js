e7156352cd9acb789ac487ab6b4e8822b60ab623
KINGMJ <328047478@qq.com>
2020-08-03 17:39:57 +0800

    mock一个项目视图对象,并绑定到vue的原型链

--- src/apps/project/core/index.js
+++ src/apps/project/core/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import router from './router'
-
+import projectJson from './mock.json'
 import Project from './Project.vue'
 
 function bootstrap () {
   Vue.config.productionTip = false
+  Vue.prototype.$project = JSON.parse(JSON.stringify(projectJson))
   new Vue({
     router,
     render: h => h(Project)
