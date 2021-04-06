b8a77876fe7fee7c9526eb530a72c25ac6227286
KINGMJ <328047478@qq.com>
2021-04-06 10:49:02 +0800

    升级到vue3

--- src/apps/project/modules/project-info/api/project.api.js
+++
@@ -1,7 +0,0 @@
-import { post } from 'api'
-
-export default {
-  edit (payload) {
-    return post('/project/edit_project_information', payload)
-  }
-}
--- src/apps/shared/constant/index.js
+++
@@ -1,3 +0,0 @@
-export default {
-  ROOT_URL: '/api'
-}
